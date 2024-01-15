903ecf818bbac21f491aac821e815911ae19b1b3
Gun <cobaltgun2324@gmail.com>
2024-01-15 14:16:53 +0700

    feat: Add complete assignment Creating APIs on Express with MongoDB

--- server/apps/products.js
+++ server/apps/products.js
@@ -1,15 +1,51 @@
 import { Router } from "express";
+import { db } from "../utils/db.js";
+import { ObjectId } from "mongodb";
 
 const productRouter = Router();
 
-productRouter.get("/", (req, res) => {});
+productRouter.get("/", async (req, res) => {
+  const collection = db.collection("Products");
+  const products = await collection.find({}).limit(10).toArray();
+
+  return res.json({ data: products });
+});
 
 productRouter.get("/:id", (req, res) => {});
 
-productRouter.post("/", (req, res) => {});
+productRouter.post("/", async (req, res) => {
+  const collection = db.collection("Products");
+  const productData = { ...req.body };
+  const products = await collection.insertOne(productData);
 
-productRouter.put("/:id", (req, res) => {});
+  return res.json({
+    message: "Product has been created successfully",
+  });
+});
 
-productRouter.delete("/:id", (req, res) => {});
+productRouter.put("/:id", async (req, res) => {
+  const collection = db.collection("Products");
+  const productId = new ObjectId(req.params.id);
+  const newProductData = { ...req.body };
+  await collection.updateOne(
+    {
+      _id: productId,
+    },
+    { $set: newProductData }
+  );
+  return res.json({
+    message: "Product has been updated successfully",
+  });
+});
+
+productRouter.delete("/:id", async (req, res) => {
+  const collection = db.collection("Products");
+  const productId = new ObjectId(req.params.id);
+
+  await collection.deleteOne({ _id: productId });
+  return res.json({
+    message: "Product has been deleted successfully",
+  });
+});
 
 export default productRouter;
